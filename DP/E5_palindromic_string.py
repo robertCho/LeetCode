bb6b96fbc9b114f400fb2298fa756f4f0f9cc8dd
RobertCho <smallbirdrober@gmail.com>
2025-05-22 21:48:07 +0800

    # 5

--- DP/E5_palindromic_string.py
+++ DP/E5_palindromic_string.py
@@ -1,21 +1,19 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        if not s: return ""
+        n = len(s)
+        if n<2:
+            return s
+        left, right = 0, 0
+        p = [[0]*n for _ in range(n)]
+        for i in range(n):
+            p[i][i] = True
+        for j in range(1,n):
+            for i in range(0,j):
+                innerIsPalind = p[i+1][j-1] or j-i<2
+                if(s[i]==s[j] and innerIsPalind):
+                    p[i][j] = True
+                    if (j-i > right -left):
+                        left = i
+                        right = j
 
-        def expand(s, left, right):
-            while left >= 0 and right < len(s) and s[left] == s[right]:
-                left -= 1
-                right += 1
-            return right - left - 1
-
-        start = end = 0
-        for i in range(len(s)):
-            len1 = expand(s, i, i)
-            len2 = expand(s, i, i + 1)
-            max_len = max(len1, len2)
-
-            if max_len > end - start:
-                start = i - (max_len - 1) // 2
-                end = i + max_len // 2
-
-        return s[start:end + 1]
+        return s[left:right+1]
